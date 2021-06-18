e2f48a71abed242be507b67e03bb227e6bb4994c
LuthfiHalim <lhluthfihalim@gmail.com>
2021-06-18 08:56:27 +0700

    add color

--- lib/misc/app_constant.dart
+++ lib/misc/app_constant.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
+
 class AppConstants {
+  static const Color COLOR_ORANGE = Color.fromRGBO(255, 172, 48, 1);
+
   //login data
   static const String LOGIN_USERNAME = "username";
   static const String LOGIN_PASSWORD = "password";
